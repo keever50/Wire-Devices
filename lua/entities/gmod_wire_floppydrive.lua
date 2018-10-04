f1e46a6a07491c42785873592fb763a5e30d1f24
Aeralius <kevinwit1999@gmail.com>
2018-10-04 16:44:50 +0200

    Again, last address was no accesable.
    Messed up logic.

--- lua/entities/gmod_wire_floppydrive.lua
+++ lua/entities/gmod_wire_floppydrive.lua
@@ -242,8 +242,8 @@ end
 --hi speed communication
 
 function ENT:ReadCell( Address )
-	if Address >= 512 and self:CheckHasFloppy() then
-		return self.buffer[Address-511]
+	if Address >= 511 and self:CheckHasFloppy() then
+		return self.buffer[Address-512]
 	else
 		--Contains floppy?
 		if Address == 0 then 
@@ -272,8 +272,8 @@ function ENT:ReadCell( Address )
 end
 
 function ENT:WriteCell( Address, value )
-	if Address >= 512 and self:CheckHasFloppy() then 
-		self.buffer[Address-511] = value
+	if Address >= 511 and self:CheckHasFloppy() then 
+		self.buffer[Address-512] = value
 	end
 		--Lock
 	if Address == 2 then
